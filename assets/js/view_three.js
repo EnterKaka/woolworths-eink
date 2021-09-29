27cccaa4e4aea450cf447ab98b1d6a420e590619
yatsukalbert@outlook.com <87792824+yatsukalbert@users.noreply.github.com>
2021-09-29 07:48:04 +0300

    sample 3d viewer

--- assets/js/view_three.js
+++ assets/js/view_three.js
@@ -1,3 +1,4 @@
+//open file dialog
 function open_model(){
     $("#input_model").trigger("click");
 }
@@ -35,87 +36,26 @@ function main() {
     const fov = 75;
     const aspect = 2;  // the canvas default
     const near = 0.1;
-    const far = 5;
+    const far = 500;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 2;
   
     const scene = new THREE.Scene();
   
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-  
-    const cubes = [];  // just an array we can use to rotate the cubes
-    const ctx = document.createElement('canvas').getContext('2d');
-    ctx.canvas.width = 25;
-    ctx.canvas.height = 25;
-    ctx.fillStyle = '#FFF';
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    const texture = new THREE.CanvasTexture(ctx.canvas);
-  
-    const material = new THREE.MeshBasicMaterial({
-      map: texture,
-    });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    cubes.push(cube);  // add to our list of cubes to rotate
-  
-    function resizeRendererToDisplaySize(renderer) {
-      const canvas = renderer.domElement;
-      const width = canvas.clientWidth;
-      const height = canvas.clientHeight;
-      const needResize = canvas.width !== width || canvas.height !== height;
-      if (needResize) {
-        renderer.setSize(width, height, false);
-      }
-      return needResize;
-    }
-  
-    function randInt(min, max) {
-      if (max === undefined) {
-        max = min;
-        min = 0;
-      }
-      return Math.random() * (max - min) + min | 0;
-    }
-  
-    function drawRandomDot() {
-      ctx.fillStyle = `#${randInt(0x1000000).toString(16).padStart(6, '0')}`;
-      ctx.beginPath();
-  
-      const x = randInt(256);
-      const y = randInt(256);
-      const radius = randInt(10, 64);
-      ctx.arc(x, y, radius, 0, Math.PI * 2);
-      ctx.fill();
-    }
-  
-    function render(time) {
-      time *= 0.001;
-  
-      if (resizeRendererToDisplaySize(renderer)) {
-        const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-      }
-  
-      drawRandomDot();
-      texture.needsUpdate = true;
-  
-      cubes.forEach((cube, ndx) => {
-        const speed = .2 + ndx * .1;
-        const rot = time * speed;
-        cube.rotation.x = rot;
-        cube.rotation.y = rot;
-      });
-  
-      renderer.render(scene, camera);
-  
-      requestAnimationFrame(render);
-    }
-  
-    requestAnimationFrame(render);
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    var animate = function () {
+      requestAnimationFrame( animate );
+    
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+    
+      renderer.render( scene, camera );
+    };
+    animate();
   }
   
   main();
