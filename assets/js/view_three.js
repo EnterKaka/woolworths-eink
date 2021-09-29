bbceb43aed44cbcd6e1aba96284415c87d1754a4
yatsukalbert@outlook.com <87792824+yatsukalbert@users.noreply.github.com>
2021-09-29 09:48:53 +0300

    random point cloud viewer

--- assets/js/view_three.js
+++ assets/js/view_three.js
@@ -1,3 +1,5 @@
+import * as THREE from './three.module.js';
+import { OrbitControls } from './OrbitControls.js';
 //open file dialog
 function open_model(){
     $("#input_model").trigger("click");
@@ -27,35 +29,83 @@ function onFileLoaded (e) {
     alert(mimeType);
     alert(content);
 }
-
+var controls, camera, renderer, scene;
 //three.js point cloud viewer
 function main() {
     const canvas = document.querySelector('#viewer_3d');
-    const renderer = new THREE.WebGLRenderer({canvas});
+    renderer = new THREE.WebGLRenderer({canvas});
   
     const fov = 75;
     const aspect = 2;  // the canvas default
     const near = 0.1;
-    const far = 500;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    const far = 1000;
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 2;
-  
-    const scene = new THREE.Scene();
-  
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    camera.position.set( 400, 200, 0 );
 
-    var animate = function () {
-      requestAnimationFrame( animate );
+    controls = new OrbitControls(camera, renderer.domElement);
+		controls.listenToKeyEvents( window ); // optional
+
+				//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+		controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+		controls.dampingFactor = 0.05;
+
+		controls.screenSpacePanning = false;
+
+		controls.minDistance = 100;
+		controls.maxDistance = 500;
+
+		controls.maxPolarAngle = Math.PI / 2;
+
+
+    scene = new THREE.Scene();
+
+    var vertices = [];
+
+    // load a resource
     
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+    // const loader = new PCDLoader();
+    // loader.load(
+    //   // resource URL
+    //   'pointcloud.pcd',
+    //   // called when the resource is loaded
+    //   function ( mesh ) {
+    //     scene.add( mesh );
+    //   },
+    //   // called when loading is in progresses
+    //   function ( xhr ) {
+    //     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    //   },
+    //   // called when loading has errors
+    //   function ( error ) {
+    //     console.log( 'An error happened' );
+    //   }
+    // );
+    for ( let i = 0; i < 10000; i ++ ) {
+      var x = THREE.MathUtils.randFloatSpread( 2000 );
+      var y = THREE.MathUtils.randFloatSpread( 2000 );
+      var z = THREE.MathUtils.randFloatSpread( 2000 );
+      vertices.push( x, y, z );
+    }
     
-      renderer.render( scene, camera );
-    };
+    var geometry = new THREE.BufferGeometry();
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+    
+    var material = new THREE.PointsMaterial( { color: 0x00ff00 } );
+    
+    var points = new THREE.Points( geometry, material );
+    
+    scene.add( points );
+    
     animate();
+    
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render( scene, camera );
   }
   
   main();
