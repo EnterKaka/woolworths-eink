5d847d2de7d39260b3cd1609ae5ed34d02312e42
yatsukalbert@outlook.com <87792824+yatsukalbert@users.noreply.github.com>
2021-10-07 22:39:16 +0300

    front design feedback

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -55,7 +55,7 @@ function drawChart(ctx,data){
         },
         title: {
             display: true,
-            text: data.name + ' - Line Chart',
+            text: data.name,
         }
     };
 
@@ -89,26 +89,37 @@ function drawChart(ctx,data){
     var lineChart = new Chart(ctx, config);
 
     //set necessay extra information
-    let lm_datetime = 'lm-datetime-' + data.name,
+    let lm_date = 'lm-date-' + data.name,
+    lm_time = 'lm-time-' + data.name,
     lm_volume = 'lm-volume-' + data.name ,
     lm_mass = 'lm-mass-' + data.name ,
     lm_density = 'lm-density-' + data.name ,
     lm_avervol = 'lm-averagevolume-' + data.name,
-    inputmodelid = 'input-modelid-' + data.name;
+    inputmodelid = 'input-modelid-' + data.name,
+    inputfromtime = 'fromtime-' + data.name,
+    inputtotime = 'totime-'  + data.name;
 
-    lm_datetime = document.getElementById(lm_datetime);
+    lm_date = document.getElementById(lm_date);
+    lm_time = document.getElementById(lm_time);
     lm_volume = document.getElementById(lm_volume);
     lm_mass = document.getElementById(lm_mass);
     lm_density = document.getElementById(lm_density);
     lm_avervol = document.getElementById(lm_avervol);
     inputmodelid = document.getElementById(inputmodelid);
+    inputfromtime = document.getElementById(inputfromtime);
+    inputtotime = document.getElementById(inputtotime);
 
-    lm_datetime.innerHTML = tempdata[2];
-    lm_volume.innerHTML = tempdata[3];
-    lm_mass.innerHTML = tempdata[4];
-    lm_density.innerHTML = tempdata[5];
-    lm_avervol.innerHTML = tempdata[6];
-    inputmodelid.value = tempdata[7];
+    lm_date.innerHTML = tempdata[2];
+    lm_time.innerHTML = tempdata[3];
+    lm_volume.innerHTML = tempdata[4];
+    lm_mass.innerHTML = tempdata[5];
+    lm_density.innerHTML = tempdata[6];
+    lm_avervol.innerHTML = tempdata[7];
+    inputmodelid.value = tempdata[8];
+    inputfromtime.value = tempdata[9];
+    inputtotime.value = tempdata[10];
+
+    // alert(tempdata[9] + '--------' + tempdata[10]);
     //ondblclick listener
     ctx.addEventListener("dblclick", function() {
         //go to 3d viewer with last id
@@ -122,8 +133,8 @@ function drawChart(ctx,data){
 
 function makeChartDataFromModelSets(data){
     let labels = [], eachdata = [];
-    let lastdatetime='1.1.1990 0:0:0',vol,mass,dens,cnt = 0,totalvols = 0, _id,
-    fromtime = Date.now(), totime = new Date(2000,1,1,0,0,0);
+    let lastdatetime=['',''],vol,mass,dens,cnt = 0,totalvols = 0, _id,
+    fromtime = new Date(), totime = new Date(2000,1,1,0,0,0);
     for(const element of data.log){
         labels.push(element.datetime);
         eachdata.push(element.volume);
@@ -133,7 +144,28 @@ function makeChartDataFromModelSets(data){
         cnt = cnt+1;
         totalvols = totalvols + parseFloat(vol);
         _id = element._id;
+
+        var tmpdate = makedefaultDate(lastdatetime);
+        tmpdate = new Date(tmpdate);
+        if(fromtime > tmpdate){
+            fromtime = tmpdate;
+        }
+        if(totime < tmpdate){
+            totime = tmpdate;
+            lastdatetime = element.datetime;
+            lastdatetime = lastdatetime.split(' ');
+        }
     }
     dens = parseFloat(mass) / parseFloat(vol);
-    return [labels, eachdata,lastdatetime,vol,mass,dens,totalvols/cnt,_id];
+    return [labels, eachdata,lastdatetime[0],lastdatetime[1],vol,mass,dens,totalvols/cnt,_id, fromtime.toISOString().slice(0,19), totime.toISOString().slice(0,19)];
+}
+
+function makedefaultDate(bugdate){
+    var truedate;
+    var tmpstr = bugdate.split(' ');
+    var tmpstr1 = tmpstr[0];
+    var list = tmpstr1.split('.');
+    truedate = list[2]+'-'+list[1]+'-'+list[0]+'T'+tmpstr[1];
+
+    return truedate;
 }
