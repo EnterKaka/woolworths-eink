8c7bb91c6bf11ddf00c230069ce3d50ae2598b99
yatsukalbert@outlook.com <87792824+yatsukalbert@users.noreply.github.com>
2021-10-08 02:15:18 +0300

    time slider complete

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -11,7 +11,7 @@ for(const name of names){
     drawChart(ctx, data);
 }
 
-function drawChart(ctx,data){
+function drawChart(ctx,data,ft,tt){
     //draw chart
         // initialize chart option
         var chartOptions = {
@@ -58,77 +58,127 @@ function drawChart(ctx,data){
             text: data.name,
         }
     };
+    if(!ft){
+        // Chart Data
+        var tempdata = makeChartDataFromModelSets(data);
+        console.log(tempdata);
+        var chartData = {
+            labels: tempdata[0],
+            datasets: [{
+                label: data.name + " - Volumes",
+                data: tempdata[1],
+                lineTension: 0,
+                fill: false,
+                borderColor: "#FF7D4D",
+                pointBorderColor: "#FF7D4D",
+                pointBackgroundColor: "#FFF",
+                pointBorderWidth: 2,
+                pointHoverBorderWidth: 2,
+                pointRadius: 4,
+            }]
+        };
 
-    // Chart Data
-    var tempdata = makeChartDataFromModelSets(data);
-    console.log(tempdata);
-    var chartData = {
-        labels: tempdata[0],
-        datasets: [{
-            label: data.name + " - Volumes",
-            data: tempdata[1],
-            lineTension: 0,
-            fill: false,
-            borderColor: "#FF7D4D",
-            pointBorderColor: "#FF7D4D",
-            pointBackgroundColor: "#FFF",
-            pointBorderWidth: 2,
-            pointHoverBorderWidth: 2,
-            pointRadius: 4,
-        }]
-    };
+        var config = {
+            type: 'line',
+            // Chart Options
+            options : chartOptions,
+            data : chartData
+        };
 
-    var config = {
-        type: 'line',
-        // Chart Options
-        options : chartOptions,
-        data : chartData
-    };
+        // Create the chart
+        var lineChart = new Chart(ctx, config);
 
-    // Create the chart
-    var lineChart = new Chart(ctx, config);
+        //set necessay extra information
+        let lm_date = 'lm-date-' + data.name,
+        lm_time = 'lm-time-' + data.name,
+        lm_volume = 'lm-volume-' + data.name ,
+        lm_mass = 'lm-mass-' + data.name ,
+        lm_density = 'lm-density-' + data.name ,
+        lm_avervol = 'lm-averagevolume-' + data.name,
+        inputmodelid = 'input-modelid-' + data.name,
+        inputfromtime = 'fromtime-' + data.name,
+        inputtotime = 'totime-'  + data.name,
+        btn = 'btn-' + data.name;
 
-    //set necessay extra information
-    let lm_date = 'lm-date-' + data.name,
-    lm_time = 'lm-time-' + data.name,
-    lm_volume = 'lm-volume-' + data.name ,
-    lm_mass = 'lm-mass-' + data.name ,
-    lm_density = 'lm-density-' + data.name ,
-    lm_avervol = 'lm-averagevolume-' + data.name,
-    inputmodelid = 'input-modelid-' + data.name,
-    inputfromtime = 'fromtime-' + data.name,
-    inputtotime = 'totime-'  + data.name;
+        lm_date = document.getElementById(lm_date);
+        lm_time = document.getElementById(lm_time);
+        lm_volume = document.getElementById(lm_volume);
+        lm_mass = document.getElementById(lm_mass);
+        lm_density = document.getElementById(lm_density);
+        lm_avervol = document.getElementById(lm_avervol);
+        inputmodelid = document.getElementById(inputmodelid);
+        inputfromtime = document.getElementById(inputfromtime);
+        inputtotime = document.getElementById(inputtotime);
+        btn = document.getElementById(btn);
 
-    lm_date = document.getElementById(lm_date);
-    lm_time = document.getElementById(lm_time);
-    lm_volume = document.getElementById(lm_volume);
-    lm_mass = document.getElementById(lm_mass);
-    lm_density = document.getElementById(lm_density);
-    lm_avervol = document.getElementById(lm_avervol);
-    inputmodelid = document.getElementById(inputmodelid);
-    inputfromtime = document.getElementById(inputfromtime);
-    inputtotime = document.getElementById(inputtotime);
+        lm_date.innerHTML = tempdata[2];
+        lm_time.innerHTML = tempdata[3];
+        lm_volume.innerHTML = tempdata[4];
+        lm_mass.innerHTML = tempdata[5];
+        lm_density.innerHTML = tempdata[6];
+        lm_avervol.innerHTML = tempdata[7];
+        inputmodelid.value = tempdata[8];
+        inputfromtime.value = tempdata[9];
+        inputtotime.value = tempdata[10];
+        
+        btn.onclick = function () {
+            let name = $(this).attr('id');
+            name = name.split('-');
+            name = name.slice(-1);
+            let inputft = 'fromtime-' + name ,inputtt = 'totime-' + name;
+            inputft = document.getElementById(inputft).value;
+            inputtt = document.getElementById(inputtt).value;
+            inputft = new Date(inputft);
+            inputtt = new Date(inputtt);
 
-    lm_date.innerHTML = tempdata[2];
-    lm_time.innerHTML = tempdata[3];
-    lm_volume.innerHTML = tempdata[4];
-    lm_mass.innerHTML = tempdata[5];
-    lm_density.innerHTML = tempdata[6];
-    lm_avervol.innerHTML = tempdata[7];
-    inputmodelid.value = tempdata[8];
-    inputfromtime.value = tempdata[9];
-    inputtotime.value = tempdata[10];
+            let canvasname = 'canvas-model-' + name;
+            let inputname = 'input-model-' + name;
+            let ctx = document.getElementById(canvasname);
+            let data = document.getElementById(inputname).value;
+            data = JSON.parse(data);
+            console.log(ctx, data);
+            drawChart(ctx, data,inputft, inputtt);
+        }
+        //ondblclick listener
+        ctx.addEventListener("dblclick", function() {
+            //go to 3d viewer with last id
+            var this_canvas = $(this).attr('id');
+            this_canvas = this_canvas.split('-');
+            this_canvas = 'input-modelid-' + this_canvas.slice(-1);
+            this_canvas = document.getElementById(this_canvas).value
+            location.href = "/data/view/" + this_canvas;
+        });
+    }else{
+        // alert('sss');
+        // Chart Data
+        var tempdata = makeChartDataFromModelSetsWithRange(data,ft,tt);
+        console.log(tempdata);
+        var chartData = {
+            labels: tempdata[0],
+            datasets: [{
+                label: data.name + " - Volumes",
+                data: tempdata[1],
+                lineTension: 0,
+                fill: false,
+                borderColor: "#FF7D4D",
+                pointBorderColor: "#FF7D4D",
+                pointBackgroundColor: "#FFF",
+                pointBorderWidth: 2,
+                pointHoverBorderWidth: 2,
+                pointRadius: 4,
+            }]
+        };
 
-    // alert(tempdata[9] + '--------' + tempdata[10]);
-    //ondblclick listener
-    ctx.addEventListener("dblclick", function() {
-        //go to 3d viewer with last id
-        var this_canvas = $(this).attr('id');
-        this_canvas = this_canvas.split('-');
-        this_canvas = 'input-modelid-' + this_canvas.slice(-1);
-        this_canvas = document.getElementById(this_canvas).value
-        location.href = "/data/view/" + this_canvas;
-    });
+        var config = {
+            type: 'line',
+            // Chart Options
+            options : chartOptions,
+            data : chartData
+        };
+
+        // Create the chart
+        var lineChart = new Chart(ctx, config);
+    }
 }
 
 function makeChartDataFromModelSets(data){
@@ -157,7 +207,29 @@ function makeChartDataFromModelSets(data){
         }
     }
     dens = parseFloat(mass) / parseFloat(vol);
+    fromtime.setMinutes(fromtime.getMinutes() - fromtime.getTimezoneOffset());
+    totime.setMinutes(totime.getMinutes() - totime.getTimezoneOffset());
     return [labels, eachdata,lastdatetime[0],lastdatetime[1],vol,mass,dens,totalvols/cnt,_id, fromtime.toISOString().slice(0,19), totime.toISOString().slice(0,19)];
+}
+
+function makeChartDataFromModelSetsWithRange(data,ft,tt){
+    let labels = [], eachdata = [];
+    // alert(JSON.stringify(data));
+    // alert(ft.toISOString());
+    // alert(tt.toISOString());
+    for(const element of data.log){
+        lastdatetime = element.datetime;
+        var tmpdate = makedefaultDate(lastdatetime);
+        tmpdate = new Date(tmpdate);
+        console.log(ft.toISOString(),tmpdate.toISOString(),tt.toISOString());
+        if(ft <= tmpdate){
+            if(tt >= tmpdate){
+                labels.push(lastdatetime);
+                eachdata.push(element.volume);
+            }
+        }
+    }
+    return [labels, eachdata];
 }
 
 function makedefaultDate(bugdate){
@@ -167,5 +239,11 @@ function makedefaultDate(bugdate){
     var list = tmpstr1.split('.');
     truedate = list[2]+'-'+list[1]+'-'+list[0]+'T'+tmpstr[1];
 
+    return truedate;
+}
+
+function makedefaultDateString(ruledate){
+    var truedate;
+    truedate = ruledate.toLocaleDateString().replace('/','-') + 'T' + ruledate.toLocaleTimeString().slice(0,8);
     return truedate;
 }
