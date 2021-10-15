d246158f7f25308418f7cd938d2866c90e3fe1bb
apollo1130 <81542987+apollo1130@users.noreply.github.com>
2021-10-15 16:04:06 +0900

    update data page

--- assets/js/alert_getmodelfromdb.js
+++ assets/js/alert_getmodelfromdb.js
@@ -1,3 +1,4 @@
+var table;
 function onclick_get(){
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
@@ -19,45 +20,68 @@ function onclick_get(){
       if (result.isConfirmed) {
         $('#table_main_data').html('');
         url = '/data/get';
-        $.post(url, function( data ) {
-          console.log(data);
-          if(data.status === 'success') {
-            let sentdata = data.data;
-            let tabletag = $('#table_main_data');
-            if(tabletag){
-              let tableinnerhtml;
-              sentdata.forEach(element => {
-                let onerow = '<tr>'+
-                '<td>' + element.name + '</td>'+
-                '<td>' + element.date + '</td>'+
-                '<td>' + element.time + '</td>'+
-                '<td>' + element.mass + '</td>'+
-                '<td>' + element.volume + '</td>'+
-                '<td>'+
-                '<div style="float:left">'+
-                '<a href="/data/view/' + element._id + '" class="btn btn-primary btn-min-width mr-1 mb-1">View</a>' +	
-                '</div>'+
-                '</td>'+
-                '</tr>';
-                tableinnerhtml += onerow;
+        table.clear().draw();
+        table.destroy();
+        $('#prepross').html('<img src="/img/loading.gif">');
+        $.post( url, function( queryResult ) {
+          table = $('table').DataTable( {
+            responsive: true,
+            autoWidth: true,
+            processing: true,
+            data: queryResult.data,
+            columns: [
+                { data: 'name',width: '25%', responsivePriority: 1, targets: 0 },
+                { data: 'date', width: '30%', targets: 1},
+                { data: 'time', width: '30%', targets: 2},
+                { data: 'mass', width: '30%', targets: 3},
+                { data: 'volume', width: '30%', targets: 4},
+                { targets: 5, render:function ( data, type, row ) {
+                  return '<a href="/data/view/' + row._id + '" class="btn btn-primary btn-min-width mr-1 mb-1">View</a>';}},
+                ],
+                initComplete: function(settings, json) {
+                  $('#prepross').empty();
+                }
               });
-              tabletag.html(tableinnerhtml);
-            }
-            swalWithBootstrapButtons.fire(
-              'Got it!',
-              'Your db is updated.',
-              'success'
-            );
-            // location.reload();
-          }else{
-            $('#table_main_data').html('');
-            swalWithBootstrapButtons.fire(
-              'Failed it!',
-              'We cannot access this database.',
-              'error'
-            );
-          }
         });
+        // $.post(url, function( data ) {
+        //   console.log(data);
+        //   if(data.status === 'success') {
+        //     let sentdata = data.data;
+        //     let tabletag = $('#table_main_data');
+        //     if(tabletag){
+        //       let tableinnerhtml;
+        //       sentdata.forEach(element => {
+        //         let onerow = '<tr>'+
+        //         '<td>' + element.name + '</td>'+
+        //         '<td>' + element.date + '</td>'+
+        //         '<td>' + element.time + '</td>'+
+        //         '<td>' + element.mass + '</td>'+
+        //         '<td>' + element.volume + '</td>'+
+        //         '<td>'+
+        //         '<div style="float:left">'+
+        //         '<a href="/data/view/' + element._id + '" class="btn btn-primary btn-min-width mr-1 mb-1">View</a>' +	
+        //         '</div>'+
+        //         '</td>'+
+        //         '</tr>';
+        //         tableinnerhtml += onerow;
+        //       });
+        //       tabletag.html(tableinnerhtml);
+        //     }
+        //     swalWithBootstrapButtons.fire(
+        //       'Got it!',
+        //       'Your db is updated.',
+        //       'success'
+        //     );
+        //     // location.reload();
+        //   }else{
+        //     $('#table_main_data').html('');
+        //     swalWithBootstrapButtons.fire(
+        //       'Failed it!',
+        //       'We cannot access this database.',
+        //       'error'
+        //     );
+        //   }
+        // });
       }
     });
   }
