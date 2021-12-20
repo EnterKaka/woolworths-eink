32f4de71dbe9f15162e808d9c514e8263e5abd2d
EnterKaka <81542987+apollo1130@users.noreply.github.com>
2021-12-20 19:05:19 +0100

    set default value

--- model/User.js
+++ model/User.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-	name: String,
-	email: String,
-	pass: String,
-	privilege: String,
+	name: {type: String, default: 'admin'},
+	email: {type: String, default: 'admin@owleyesystem.com'},
+	pass: {type: String, default: 'OwlEyeAdmin'},
+	privilege: {type: String, default: 'admin'},
 });
 
 module.exports = mongoose.model('users', UserSchema);
